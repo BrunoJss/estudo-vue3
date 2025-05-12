452a7bd524062bc8fd1ce8719639a5ef4929ce7c
Bruno Cruz <bruno.jsscruz@gmail.com>
2025-05-12 13:58:57 -0300

    Aula 3 - Criação do componente TheHeader e importação dele no App.vue. Definição de SFC (Single File Component)

---
+++ app/src/components/TheHeader.vue
@@ -0,0 +1,19 @@
+<template>
+    <header class="header">
+        Header
+    </header>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+ .header{
+    font-size: 32px;
+    background: #000;
+    color: aliceblue;
+ }
+</style>
