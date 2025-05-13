91bb9e948f61654146c5caa10e35185a7e0c5c04
Bruno Cruz <bruno.jsscruz@gmail.com>
2025-05-13 09:17:14 -0300

    Aula 4 - Definição de diretivas e apresentação das diretivas v-if e v-show. Primeira vez utilizando a função data para declarar variáveis

--- app/src/App.vue
+++ app/src/App.vue
@@ -1,6 +1,19 @@
 <template>
   <!-- <TheHeader></TheHeader>  Nota: Dos dois jeitos funcionam nesse caso-->
-  <TheHeader />
+  <!--  v-show altera a propriedade de estilo para display:none, ou seja, o elemento existe no DOM diferente do v-if-->
+  <TheHeader 
+    v-if="showHeader"
+  />
+  <div v-show="showName">
+    Nome: {{ firstName }}
+    <br>
+    Sobrenome: {{ lastName }}
+  </div>
+
+  <div v-if="accessLevel === 'Admin'">Admin</div>
+  <div v-else-if="accessLevel === 'Marketing'">Marketing</div>
+  <div v-else>User</div>
+
   <img alt="Vue logo" src="./assets/logo.png">
   <HelloWorld msg="Welcome to Your Vue.js App" />
 </template>
@@ -14,6 +27,15 @@ export default {
   components: {
     HelloWorld,
     TheHeader
+  },
+  data(){
+    return{
+      showHeader: true,
+      firstName: "Bruno",
+      lastName: "Cruz",
+      showName: false,
+      accessLevel: 'Admin'
+    }
   }
 }
 </script>
